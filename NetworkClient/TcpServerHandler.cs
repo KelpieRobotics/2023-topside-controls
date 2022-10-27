dee027adfc3e7cad3438def0629ac8bdc43d6e19
jchiedrap <jchiedrap@gmail.com>
2022-10-27 14:25:10 -0400

    Added project file, namespace, base file.

---
+++ NetworkClient/TcpServerHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace NetworkClient;
+
+public class TcpServerHandler
+{
+    
+}

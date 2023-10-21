69be71e286fa2e3f015028e29956dfb25cb2b24d
Umer Qamar <114540422+DevFate@users.noreply.github.com>
2023-10-21 15:31:27 -0400

    Add files via upload

---
+++ 2023-topside-controls/NetworkClient/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+class Ignore
+{
+	void whatever() // this will never run, just for reference
+	{
+		using var client = new TcpClient();
+
+		IPAddress hostname = IPAddress.Parse("10.192.77.99");
+		client.Connect(hostname, 9999);
+
+		using NetworkStream stream = client.GetStream();
+
+		while (true)
+		{
+			var message = Console.ReadLine();
+
+			if (!String.IsNullOrEmpty(message))
+			{
+				message += "\r\n";
+				byte[] bytes = Encoding.ASCII.GetBytes(message);
+				stream.Write(bytes, 0, bytes.Length);
+			}
+			else if (message == "quit")
+			{
+				client.Close();
+				break;
+			}
+
+			if (stream.DataAvailable)
+			{
+
+				byte[] readBuffer = new byte[1024];
+				StringBuilder completeMessage = new StringBuilder();
+				stream.ReadTimeout = 250;
+				try
+				{ // try while the length of the read !=0
+					int numberOfBytesRead;
+					while ((numberOfBytesRead = stream.Read(readBuffer, 0, readBuffer.Length)) > 0)
+					{
+						Console.WriteLine(stream.DataAvailable);
+						completeMessage.Append(Encoding.UTF8.GetString(readBuffer, 0, numberOfBytesRead));
+						Console.WriteLine(completeMessage);
+					}
+				}
+				catch (IOException)
+				{
+					Console.WriteLine("Transmission Finished...");
+				}
+			}
+		}
+	}
+}
+
+
